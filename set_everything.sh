b72748e2b2bf0fab5c82a40fb7cf81f03adc4d55
Joao Trindade <trindade.joao@gmail.com>
2010-04-28 11:07:42 +0100

    Working on the set everything script

--- set_everything.sh
+++ set_everything.sh
@@ -1,8 +1,14 @@
+#Get the number of the arm
+NUMBER=`echo $HOSTNAME | tr -d arm `
+
 #cp the module to the right place
-cp rt73_ts7500.ko /lib/modules/2.6.24.4/kernel/drivers/net/wireless/rt2x00/
-depmod -a
+#cp rt73_ts7500.ko /lib/modules/2.6.24.4/kernel/drivers/net/wireless/rt2x00/
+#depmod -a
 
 #copy the modprobe blacklist blacklist
-cp blacklist /etc/modprobe.d/blacklist
+#cp blacklist /etc/modprobe.d/blacklist
 
 #cp the network interfaces config file
+TO_EXECUTE="sed \"s/#CHANGE_NUMBER#/$NUMBER/g\" interfaces_global > interfaces"
+`$TO_EXECUTE`
+cp interfaces /etc/network/interfaces
