579bbfae9da153123d86de97d7bf2ef4a03f9f83
Joao Trindade <trindade.joao@gmail.com>
2010-04-28 15:19:44 +0100

    Did a little trick to correct channel

--- adhoc.sh
+++ adhoc.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+ifconfig $1 up
+iwconfig $1 mode managed
+sleep 3
+ifconfig $1 down
 ifconfig $1 up
 iwconfig $1 mode ad-hoc essid teste channel 1 ap 02:0C:F1:B5:CC:5D
-ifconfig $1 192.168.0.$2
+ifconfig $1 $2
