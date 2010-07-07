44925902281b151a4f9101090fa7b8b50f403ce5
Frederico Gonçalves <frederico.lopes.goncalves@gmail.com>
2010-07-07 17:28:39 +0100

    Added placas

---
+++ placas7500/check_all.sh
@@ -0,0 +1,9 @@
+#Check if the module is loaded
+
+#Check if the device is correct
+
+#Check if the ip is correct
+
+#Check if the link is ok
+
+#Check if the script for adhoc communication is present
